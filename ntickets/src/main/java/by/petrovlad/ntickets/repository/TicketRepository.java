23dd9fbc61197006201a218456c8218300765119
vladi <vladislav_petrovec@mail.ru>
2021-08-12 01:39:00 +0300

    added annotation @transactional so now it works fine

--- ntickets/src/main/java/by/petrovlad/ntickets/repository/TicketRepository.java
+++ ntickets/src/main/java/by/petrovlad/ntickets/repository/TicketRepository.java
@@ -2,12 +2,14 @@ package by.petrovlad.ntickets.repository;
 
 import by.petrovlad.ntickets.model.entity.Ticket;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface TicketRepository extends CrudRepository<Ticket, Long> {
     Iterable<Ticket> findAllByAuthorId(Long authorId);
     Optional<Ticket> findByUniqueHash(String hash);
+    @Transactional
     void deleteByUniqueHash(String hash);
     boolean existsByUniqueHash(String hash);
 }
