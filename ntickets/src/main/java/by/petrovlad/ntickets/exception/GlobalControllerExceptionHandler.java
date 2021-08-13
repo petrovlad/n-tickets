f3fab9fc951b059f118c154142f15e8b413c02ed
vladi <vladislav_petrovec@mail.ru>
2021-08-13 10:24:07 +0300

    added some exception handlers

--- ntickets/src/main/java/by/petrovlad/ntickets/exception/GlobalControllerExceptionHandler.java
+++ ntickets/src/main/java/by/petrovlad/ntickets/exception/GlobalControllerExceptionHandler.java
@@ -2,9 +2,15 @@ package by.petrovlad.ntickets.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
@@ -14,5 +20,29 @@ public class GlobalControllerExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public void handlerResourceNotFound(ResourceNotFoundException exception) {
         log.error(exception.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(UserExistsException.class)
+    public void handlerUserExists(UserExistsException exception) {
+        log.error(exception.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpClientErrorException.class)
+    public void handlerHttpClientError(HttpClientErrorException exception) {
+        log.error(exception.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
